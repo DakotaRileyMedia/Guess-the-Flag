8782799f506884be0313fb9f31c256e3e4d59cd1
DakotaRileyMedia <dakotacustompcs@gmail.com>
2022-04-20 15:57:37 -0400

    Added animations, tweaked the gameplay
    I added a few animations that make the game look better, and I also tweaked the gameplay so that flags don't repeat in the same set of questions

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -9,13 +9,19 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var showingScore = false
-    @State private var isGameComplete = false
+    @State private var showingResults = false
+    
     @State private var scoreTitle = ""
-    @State private var message = ""
-    @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy","Monaco", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
-    @State private var correctAnswer = Int.random(in: 0...2)
+    
     @State private var score = 0
-    @State private var flagTappedCounter = 0
+    @State private var questionCounter = 1
+    
+    @State private var countries = allCountries.shuffled()
+    @State private var correctAnswer = Int.random(in: 0...2)
+    
+    @State private var selectedFlag = -1
+    
+    static let allCountries = ["Estonia", "France", "Germany", "Ireland", "Italy","Monaco", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"]
     
     struct FlagImage: View {
         var country: String
@@ -25,6 +31,7 @@ struct ContentView: View {
                 .renderingMode(.original)
                 .clipShape(RoundedRectangle(cornerRadius: 20))
                 .shadow(radius: 5)
+                .padding(15)
         }
     }
     
@@ -53,11 +60,16 @@ struct ContentView: View {
                     
                     ForEach(0..<3) { number in
                         Button {
-                                flagTapped(number)
+                            flagTapped(number)
                         } label: {
                             FlagImage(country: countries[number])
+                                .rotation3DEffect(.degrees(selectedFlag == number ? 360 : 0), axis: ( x:0, y:1, z:0 ))
+                                .opacity(selectedFlag == -1 || selectedFlag == number ? 1 : 0.25)
+                                .saturation(selectedFlag == -1 || selectedFlag == number ? 1 : 0)
+                                .scaleEffect(selectedFlag == -1 || selectedFlag == number ? 1 : 0.75)
+                                .blur(radius: selectedFlag == -1 || selectedFlag == number ? 0 : 3)
+                                .animation(.default, value: selectedFlag)
                         }
-                        .padding(15)
                     }
                 }
                 .frame(maxWidth: .infinity)
@@ -79,43 +91,57 @@ struct ContentView: View {
         .alert(scoreTitle, isPresented: $showingScore) {
             Button("Continue", action: askQuestion)
         } message: {
-            Text(message)
+            Text("Your score is \(score)")
         }
         
-        .alert(scoreTitle, isPresented: $isGameComplete) {
-            Button("Restart", action: reset)
+        .alert("Game Over!", isPresented: $showingResults) {
+            Button("Start Again", action: newGame)
         } message: {
-            Text("You completed the game. Your score is \(score) out of 8.")
+            Text("Your final score was \(score)")
         }
     }
     
     func flagTapped(_ number: Int) {
+        selectedFlag = number
+        
         if number == correctAnswer {
             scoreTitle = "Correct"
             score += 1
-            message = "Your score is \(score)"
         } else {
-            message = "Wrong! That’s the flag of \(countries[number])."
-            scoreTitle = "Wrong"
+            let needsThe = ["UK", "US"]
+            let answer = countries[number]
+            
+            if needsThe.contains(answer) {
+                scoreTitle = "Wrong! That's the flag of the \(answer)"
+            } else {
+                scoreTitle = "Wrong! That's the flag of \(answer)"
+            }
+                        
+            if score > 0 {
+                score -= 1
+            }
         }
-        showingScore = true
-        flagTappedCounter += 1
-    }
-    
-    func reset() {
-        score = 0
-        flagTappedCounter = 0
-        isGameComplete = false
-        askQuestion()
+        
+        if questionCounter == 8 {
+            showingResults = true
+        } else {
+            showingScore = true
+        }
     }
     
     func askQuestion() {
-        if (flagTappedCounter == 8 ) {
-            isGameComplete = true
-        } else {
-            countries.shuffle()
-            correctAnswer = Int.random(in: 0...2)
-        }
+        countries.remove(at: correctAnswer)
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
+        questionCounter += 1
+        selectedFlag = -1
+    }
+    
+    func newGame() {
+        questionCounter = 0
+        score = 0
+        countries = Self.allCountries
+        askQuestion()
     }
 }
 
